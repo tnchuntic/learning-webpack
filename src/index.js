e851fc27ca4f61cd5e3381b26687fdad659ce3a8
Thomas Chuntic <tnchuntic@hotmail.com>
2017-07-26 14:55:31 +1000

    add icons and fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
-import './style.css';
+import './css/style.css';
+import Icon from './img/github-512.png';
+import Logo from './img/webpack.svg';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +9,16 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  var webpackLogo = new Image();
+  webpackLogo.src = Logo;
+
+  element.appendChild(myIcon);
+  element.appendChild(webpackLogo);
 
   return element;
 }
